979c80e68c7bb4c5bc266c40b0bc32fd6f56aa2b
Allan Nava <allannava95@gmail.com>
2020-04-23 14:15:01 +0200

    Update heroes_controller.dart

--- lib/controller/heroes_controller.dart
+++ lib/controller/heroes_controller.dart
@@ -19,9 +19,19 @@ class HeroesController extends Controller {
     {'id': 14, 'name': 'Celeritas'},
     {'id': 15, 'name': 'Magneta'},    
   ];
-
+  // In just a moment, we'll replace this code with something even better,
+  // but it's important to understand where this information comes from first!
   @override
   Future<RequestOrResponse> handle(Request request) async {
+    if (request.path.variables.containsKey('id')) {
+    final id = int.parse(request.path.variables['id']);
+    final hero = _heroes.firstWhere((hero) => hero['id'] == id, orElse: () => null);
+    if (hero == null) {
+      return Response.notFound();
+    }
+
+    return Response.ok(hero);
+  }
     return Response.ok(_heroes);
   }
 }
